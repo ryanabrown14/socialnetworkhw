5d15b7b45c4b0eee36a6261c4f1ea63b6a69f4ed
ry brown <ryan.alex.brown@gmail.com>
2020-12-06 00:04:27 -0700

    models

---
+++ models/Reaction.js
@@ -0,0 +1,31 @@
+const {Schema, model, Types} = require('mongoose');
+
+const reactionSchema = new Schema ({
+    reactionId: {
+        type: Types.ObjectId,
+        default: new Types.ObjectId()
+    },
+    reactionBody: {
+        type: String,
+        required:true,
+        maxlength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default:  Date.now,
+        //get:
+    }
+},
+{
+    toJSON: {
+        getters: true
+    },
+    id: false
+}
+);
+
+module.exports = reactionSchema;
---
+++ models/User.js
@@ -0,0 +1,38 @@
+const {Schema, model} = require('mongoose');
+const { type } = require('os');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/]
+    },
+    thoughts: [{
+        type: Schema.Types.ObjectId, ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId, ref: 'User'
+    }]
+    
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+}
+);
+
+userSchema.virtual('friendCount').get(function(){
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+module.exports = User;
